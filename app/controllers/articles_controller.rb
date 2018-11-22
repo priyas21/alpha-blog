c699ae455f3e153bb1b5d8c237ac008719c4afdd
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-11-22 16:59:47 +1300

    Add the functionality to edit and update the articles

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -14,6 +14,18 @@ class ArticlesController < ApplicationController
   def show
     @article = Article.find(params[:id])
   end
+  def edit
+    @article = Article.find(params[:id])
+   end
+  def update
+    @article = Article.find(params[:id])
+    if @article.update(article_params)
+      flash[:notice] ="Article was updated successfully"
+      redirect_to article_path(@article)
+    else
+      render 'edit'
+    end
+  end
 
   private
   def article_params
