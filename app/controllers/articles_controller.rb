1b118e22aa40ea4d4232f045cb62eb6c69642516
Priya Sodhi <priya.sodhi@fluxfederation.com>
2018-11-22 16:13:09 +1300

    AB-01 Create and show the articles
    Add the capability to display success message when the article is created without errors
    Dsiplay the error messages when the validations are not passed
    Displat the recdnlty created article

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,11 +4,15 @@ class ArticlesController < ApplicationController
   end
   def create
     @article = Article.new(article_params)
-    @article.save
-    redirect_to article_path(@article)
+    if @article.save
+      flash[:notice] = "Article was successfully created"
+      redirect_to article_path(@article)
+    else
+      render 'new'
+    end
   end
   def show
-
+    @article = Article.find(params[:id])
   end
 
   private
