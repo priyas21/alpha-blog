155528c8e528c2634eb93bd49c0477a7d1b7bde4
Priya Sodhi <priya.sodhi@fluxfederation.com>
2019-03-21 10:33:39 +1300

    AB-01 Allow user to update their details

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,7 @@
 class UsersController < ApplicationController
 
+  before_action :set_user, only: [:edit, :update]
+
   def new
     @user = User.new
   end
@@ -15,8 +17,25 @@ class UsersController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if @user.update(user_params)
+      flash[:success] =" Your account was updated successfully."
+      redirect_to articles_path
+    else
+      render 'edit'
+    end
+
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email, :password)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
   end
 end
