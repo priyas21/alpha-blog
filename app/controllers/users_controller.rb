9bb29b52b662e671893fa187bff2ab72a3886356
Priya Sodhi <priya.sodhi@fluxfederation.com>
2019-03-25 11:52:20 +1300

    AB-01 Remove the comment

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,6 @@ class UsersController < ApplicationController
   end
 
   def create
-    # debugger
     @user = User.new(user_params)
     if @user.save
       flash[:success] = "Welcome to food blog #{@user.username.capitalize}"
