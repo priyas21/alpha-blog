b5cb47545b9f303a28d8f2ee7a7cd37fa0558b23
Priya Sodhi <priya.sodhi@fluxfederation.com>
2019-05-01 12:47:53 +1200

    AB-01 Allow admin to delete other users and their articles

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-  before_action :set_user, only: [:edit, :update, :show]
-  before_action :require_current_user, only: [:edit, :update]
+  before_action :set_user, only: [:edit, :update, :show, :destroy]
+  before_action :require_current_user, only: [:edit, :update, :destroy]
+  before_action :require_admin, only: [:destroy]
 
   def index
     @users = User.paginate(page: params[:page], per_page: 5)
@@ -37,6 +38,12 @@ class UsersController < ApplicationController
     @user_articles = @user.articles.paginate(page: params[:page], per_page: 2)
   end
 
+  def destroy
+    @user.destroy
+    flash[:danger] = "User was successfully deleted"
+    redirect_to users_path
+  end
+
   private
   def user_params
     params.require(:user).permit(:username,:email, :password)
@@ -47,9 +54,17 @@ class UsersController < ApplicationController
   end
 
   def require_current_user
-    if current_user != @user
+    if current_user != @user && !current_user.admin?
       flash[:danger] = "You can only edit your own account"
       redirect_to root_path
     end
   end
+
+  def require_admin
+    if !logged_in? && !current_user.admin?
+      flash[:danger] = "Only admins are allowed to do this action"
+      redirect_to root_path
+    end
+  end
+
 end
